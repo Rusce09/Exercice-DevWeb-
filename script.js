7722d6addc4f64fda2f0a3308893e5d711293c5f
Rusce09 <roxane.borgeaud@unil.ch>
2023-10-19 22:37:02 +0200

    hhj

---
+++ script.js
@@ -0,0 +1,5 @@
+let superlien = document.createElement ("a")
+superlien.setAttribute ("href", "https://unil.ch")
+superlien.innerHTML = "cliquez ici"
+superlien.classList.add("couleurRouge")
+document.body.appendChild(superlien)
