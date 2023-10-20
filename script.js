6d0de9682a750fefcbb6544958bf582ea1b4b3f8
Rusce09 <roxane.borgeaud@unil.ch>
2023-10-20 21:57:52 +0200

    gfgf

--- script.js
+++ script.js
@@ -9,10 +9,15 @@ newlien.setAttribute ("href", "https://github.com")
 newlien.innerHTML= "cliquez là"
 document.body.appendChild(newlien)
 
-let monimage = document.createElement ("imag")
-monimage.setAttribute("src, https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png")
+let monimage = document.createElement("img")
+monimage.setAttribute("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png")
 document.body.appendChild(monimage)
 
 let monparagraphe = document.createElement("p")
-monparagraphe.innerHTML= "je déteste le covid"
+monparagraphe.innerHTML="je déteste le covid"
 document.body.appendChild(monparagraphe)
+
+let monbr = document.createElement("br")
+document.body.appendChild(monbr)
+
+
