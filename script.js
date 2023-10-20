b39a2efb22aa8c7c62513dd543a878d4d8134142
Rusce09 <roxane.borgeaud@unil.ch>
2023-10-20 21:48:39 +0200

    fhgdcfg

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ superlien.setAttribute ("href", "https://unil.ch")
 superlien.innerHTML = "cliquez ici"
 superlien.classList.add("couleurRouge")
 document.body.appendChild(superlien)
+
+let newlien = document.createElement("a")
+newlien.setAttribute ("href", "https://github.com")
+newlien.innerHTML= "cliquez là"
+document.body.appendChild(newlien)
+
+let monimage = document.createElement ("imag")
+monimage.setAttribute("src, https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png")
+document.body.appendChild(monimage)
+
+let monparagraphe = document.createElement("p")
+monparagraphe.innerHTML= "je déteste le covid"
+document.body.appendChild(monparagraphe)
